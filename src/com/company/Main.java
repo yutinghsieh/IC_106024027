1a6d1e58fc4547c76f63e042fb8954274ce9cd1c
yutinghsieh <hsieh_9382@yahoo.com>
2017-09-25 14:50:33 +0800

    modified

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("106024027謝雨庭");
+        System.out.println("資傳系");
+    }
+}
